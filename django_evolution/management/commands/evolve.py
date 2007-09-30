95662c5355864f621749bf94903a2cd91d0c52cf
Russell Keith-Magee <russell@keith-magee.com>
2007-09-30 04:34:02 +0000

    Cleanup of output of evolve command.

--- django_evolution/management/commands/evolve.py
+++ django_evolution/management/commands/evolve.py
@@ -31,7 +31,7 @@ class Command(BaseCommand):
     requires_model_validation = False
 
     def handle(self, *args, **options):
-        interactive = options.get('interactive')
+        verbosity = int(options['verbosity'])
         for app in get_apps():
             app_name = '.'.join(app.__name__.split('.')[:-1])
             app_sig = create_app_sig(app)
@@ -42,7 +42,8 @@ class Command(BaseCommand):
                 last_evolution = evolutions[0]
                 if last_evolution.signature != signature:
                     # Migration Required. Evolve the model.
-                    print 'Migration Required - %s' % app_name
+                    if verbosity > 1:
+                        print 'Application %s requires evolution' % app_name
                     last_evolution_sig = pickle.loads(str(last_evolution.signature))
                     
                     if options['hint']:
@@ -64,32 +65,44 @@ class Command(BaseCommand):
                             transaction.commit()
                         except Exception, ex:
                             transaction.rollback()
-                            print self.style.ERROR('Error during evolution: %s' % str(ex))
+                            print self.style.ERROR('Error during evolution of %s: %s' % (app_name, str(ex)))
                             sys.exit(1)
                         # Now update the evolution table
                         if options['hint']:
                             version = None
                         else:
                             version = last_evolution.version + 1
+                        print "UPDATE EVO TABLE"
                         new_evolution = Evolution(app_name=app_name,
                                                   version=version,
                                                   signature=signature)
                         new_evolution.save()
-                        print 'Evolution Successful'
+                        print new_evolution
+                        print new_evolution.id
+                        print Evolution.objects.count(), Evolution.objects.all()
                     else:
                         if options['compile']:
+                            print ';; Compiled evolution SQL for %s' % app_name 
                             for s in sql:
-                                print s
+                                print s                            
                         elif options['hint']:
-                            # FIXME - this isn't particularly meaningful
+                            print '--- Evolution hint for %s -------------------' % app_name
+                            print 'from %s import *' % app_name
+                            print 
                             print 'MUTATIONS = ['
                             for m in mutations:
                                 print '    ',m
                             print ']'
-                        else:
-                            print 'Trial Evolution Successful'
                 else:
-                    print 'Evolution not required for app %s' % app_name
+                    if verbosity > 1:
+                        print 'Application %s is up to date' % app_name
             else:
-                print "Can't evolve yet. Need to set a baseline syncdb."
+                print self.style.ERROR("Can't evolve yet. Need to set a baseline for %s." % app_name)
                 sys.exit(1)
+        if options['execute']:
+            if verbosity > 0:
+                print 'Evolution successful.'
+        elif not options['compile'] and not options['hint']:
+            if verbosity > 0:
+                print "Trial evolution successful. Run './manage.py evolve --execute' to apply evolution."
+        print Evolution.objects.count(), Evolution.objects.all()
--- django_evolution/models.py
+++ django_evolution/models.py
@@ -6,13 +6,16 @@ class Evolution(models.Model):
     version = models.PositiveIntegerField(null=True, blank=True)
     signature = models.TextField()
     when = models.DateTimeField(auto_now=True)
-    
+
+    class Admin:
+        pass
+        
     class Meta:
         ordering = ('when',)
         db_table = 'django_evolution'
 
     def __unicode__(self):
         if self.version is None:
-            return u'Temporary %s version, applied %s' % (self.app_name,self.date)
+            return u'Temporary %s version, applied %s' % (self.app_name,self.when)
         else:
-            return u'%s Version %d, applied %s' % (self.app_name,self.version,self.date)
+            return u'%s Version %d, applied %s' % (self.app_name,self.version,self.when)
