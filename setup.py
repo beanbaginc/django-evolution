d39bea820176608acd7cb2ef27131f9507398013
Christian Hammond <chipx86@chipx86.com>
2010-04-19 02:13:17 +0000

    Add setuptools support and a version number.
    This moves the packaging over to setuptools, giving us the ability to
    create eggs.
    
    It also gives Django Evolution a version number, which I've arbitrary
    marked as 0.2.

--- setup.py
+++ setup.py
@@ -1,17 +1,49 @@
-from distutils.core import setup
+#!/usr/bin/env python
+#
+# Setup script for Django Evolution
 
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test
+
+import django_evolution
+
+
+def run_tests(*args):
+    import os
+    os.system('tests/runtests.py')
+
+test.run_tests = run_tests
+
+
+PACKAGE_NAME = 'django_evolution'
+
+
+# Build the package
 setup(
-    name='django_evolution',
-    description='An implementation of schema evolution for the Django web framework.',
+    name=PACKAGE_NAME,
+    version=django_evolution.__version__,
+    description='A database schema evolution tool for the Django web framework.',
+    url='http://code.google.com/p/django-evolution/',
     author='Ben Khoo',
     author_email='khoobks@westnet.com.au',
-    url='http://code.google.com/p/django-evolution/',
-    packages=[
-        'django_evolution',
-        'django_evolution.db',
-        'django_evolution.management',
-        'django_evolution.management.commands',
-        'django_evolution.tests',
-        'django_evolution.tests.db',
+    maintainer='Christian Hammond',
+    maintainer_email='chipx86@chipx86.com',
+    packages=find_packages(),
+    install_requires=[
+        'Django>=1.1.1',
     ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
