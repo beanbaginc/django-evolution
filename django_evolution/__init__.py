38bf5a14b0a20e99da9c8133402f21269348d38b
Christian Hammond <chipx86@chipx86.com>
2010-10-25 09:31:29 +0000

    Bump to 0.6.2 dev.

--- django_evolution/__init__.py
+++ django_evolution/__init__.py
@@ -4,7 +4,7 @@
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 6, 1, 'final', 0, True)
+VERSION = (0, 6, 2, 'alpha', 0, False)
 
 
 def get_version_string():
