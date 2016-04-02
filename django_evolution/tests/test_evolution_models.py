2809f97696791f34e659dc9fc3c9e29c802082d2
Christian Hammond <christian@beanbaginc.com>
2016-04-01 18:56:08 -0700

    Fix unit tests for Version.objects.get_current().
    The unit tests assumed that no Version objects were present, but this
    wasn't always the case. This clears them before the test runs.

--- django_evolution/tests/test_evolution_models.py
+++ django_evolution/tests/test_evolution_models.py
@@ -10,6 +10,9 @@ class VersionManagerTests(TestCase):
 
     def test_current_version_with_dup_timestamps(self):
         """Testing Version.current_version() with two entries with same timestamps"""
+        # Remove anything that may already exist.
+        Version.objects.all().delete()
+
         timestamp = datetime(year=2015, month=12, day=10, hour=12, minute=13,
                              second=14)
 
