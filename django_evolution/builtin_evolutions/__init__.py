41bdb951e61f5601c380fd81690d74d35136455f
Christian Hammond <christian@beanbaginc.com>
2017-03-09 14:43:15 -0800

    Fix a field lookup to be compatible with all versions of Django.
    Recent versions of Django removed Meta.get_field_by_name(), which our
    built-in evolutions were trying to use. Fortunately, all supported
    versions have Meta.get_field(), which works just as well. This change
    switches over to that.
    
    We'll want to revisit the built-in evolutions before release in order to
    prevent possible conflicts with Django's migrations.

--- django_evolution/builtin_evolutions/__init__.py
+++ django_evolution/builtin_evolutions/__init__.py
@@ -11,7 +11,7 @@ BUILTIN_SEQUENCES = {
 # Starting in Django 1.3 alpha, Session.expire_date has a db_index set.
 # This needs to be reflected in the evolutions. Rather than hard-coding
 # a specific version to check for, we check the actual value in the field.
-if Session._meta.get_field_by_name('expire_date')[0].db_index:
+if Session._meta.get_field('expire_date').db_index:
     BUILTIN_SEQUENCES['django.contrib.sessions'].append(
         'session_expire_date_db_index')
 
