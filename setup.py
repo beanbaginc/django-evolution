cbd30c86e7db8ca0161e4b43216dbd7ee22eda7c
David Trowbridge <trowbrds@gmail.com>
2018-01-20 11:33:12 -0500

    Nuke ancient ez_setup.py file.
    django-evolution was still using the ancient, deprecated ez_setup
    bootstrap. This hasn't been necessary for a long time, and breaks with
    modern versions of things.
    
    Testing done:
    Ran setup.py develop and saw that things installed correctly on both
    python 2.7 and 3.6.
    
    Reviewed at https://reviews.reviewboard.org/r/9505/

--- setup.py
+++ setup.py
@@ -2,9 +2,6 @@
 #
 # Setup script for Django Evolution
 
-from ez_setup import use_setuptools
-use_setuptools()
-
 from setuptools import setup, find_packages
 from setuptools.command.test import test
 
