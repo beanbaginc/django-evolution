4e507de814f8755e50618ed770b098b098fb0107
Christian Hammond <christian@beanbaginc.com>
2019-06-14 02:57:19 -0700

    Bump to Django Evolution 2.0 dev.
    Django Evolution has matured and changed a lot, and we want a version
    number that reflects this new evolution (ahem) of the module. We'll be
    releasing this as Django Evolution 2.0, instead of 0.8 as it was
    previously versioned.

--- django_evolution/__init__.py
+++ django_evolution/__init__.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals
 #
 #   (Major, Minor, Micro, alpha/beta/rc/final, Release Number, Released)
 #
-VERSION = (0, 8, 0, 'alpha', 0, False)
+VERSION = (2, 0, 0, 'alpha', 0, False)
 
 
 def get_version_string():
