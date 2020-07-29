e4c02d33cc7288941bfaa85cd35ebbd80a4a8c5c
Christian Hammond <christian@beanbaginc.com>
2020-07-28 19:15:50 -0700

    Add a compatibility patch for Django 2.0's SQLite unique index name quoting.
    Django 2.0 had a bug when generating SQL for creating a unique index
    where the name wasn't being quoted. This could result in statements with
    syntax errors, and impacted some unit tests.
    
    This change adds a runtime compatibility patch that overrides Django
    2.0's `_create_unique_sql()` that adds quoting, if code is detected that
    would skip the quoting.
    
    This won't impact any version of Django that has the correct quoting
    behavior.
    
    Testing Done:
    Unit tests pass on Django 2.0.
    
    Reviewed at https://reviews.reviewboard.org/r/11085/

--- django_evolution/compat/patches/__init__.py
+++ django_evolution/compat/patches/__init__.py
@@ -2,11 +2,14 @@
 
 from __future__ import unicode_literals
 
-from django_evolution.compat.patches import sqlite_legacy_alter_table
+from django_evolution.compat.patches import (
+    django2_0_quote_unique_index_name,
+    sqlite_legacy_alter_table)
 
 
 #: List of patches that can be applied.
 patches = [
+    django2_0_quote_unique_index_name,
     sqlite_legacy_alter_table,
 ]
 
---
+++ django_evolution/compat/patches/django2_0_quote_unique_index_name.py
@@ -0,0 +1,54 @@
+"""Patch to add missing unique index name quoting on Django 2.0.x."""
+
+from __future__ import unicode_literals
+
+try:
+    # Django >= 2.0
+    from django.db.backends.base.schema import BaseDatabaseSchemaEditor
+    from django.db.backends.ddl_references import IndexName
+except ImportError:
+    # Django < 1.7
+    BaseDatabaseSchemaEditor = None
+    IndexName = None
+
+
+def needs_patch():
+    """Return whether the unique index name generation needs patching.
+
+    It will need patching if the SchemaEditor has a ``_create_unique_sql``
+    method (added on Django 2.0.x, removed for 2.1.0).
+
+    Returns:
+        bool:
+        ``True`` if the backend needs to be patched. ``False`` if it does not.
+    """
+    return (IndexName is not None and
+            hasattr(BaseDatabaseSchemaEditor, '_create_unique_sql'))
+
+
+def apply_patch():
+    """Apply a patch to the base schema editor.
+
+    This will override the ``_create_unique_sql()`` method, which generates
+    the SQL for a ``CREATE UNIQUE INDEX`` statement, forcing the index name
+    to be quoted. This is common across all Django database backends.
+    """
+    assert BaseDatabaseSchemaEditor is not None
+
+    def _create_unique_sql(self, *args, **kwargs):
+        from django.db.backends.ddl_references import IndexName
+
+        statement = orig_create_unique_sql(self, *args, **kwargs)
+
+        index_name = statement.parts['name']
+        assert isinstance(index_name, IndexName)
+
+        if index_name.create_index_name == self._create_index_name:
+            # The result will be unquoted. Let's quote it.
+            index_name.create_index_name = lambda *args, **kwargs: \
+                self.quote_name(self._create_index_name(*args, **kwargs))
+
+        return statement
+
+    orig_create_unique_sql = BaseDatabaseSchemaEditor._create_unique_sql
+    BaseDatabaseSchemaEditor._create_unique_sql = _create_unique_sql
